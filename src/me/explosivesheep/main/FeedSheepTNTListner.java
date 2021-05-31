38b9fc55d3e42a0ac858ec584872a5e534b81f74
timsixth <timsixth@op.pl>
2021-05-31 21:00:14 +0200

    Spawned more than one sheep fixed 1.16.5 support added

--- src/me/explosivesheep/main/FeedSheepTNTListner.java
+++ src/me/explosivesheep/main/FeedSheepTNTListner.java
@@ -13,38 +13,36 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class FeedSheepTNTListner implements Listener {
 
-	private final ConfigFile cfg;
+		private final ConfigFile cfg;
 
-	public FeedSheepTNTListner(ConfigFile configFile) {
-		this.cfg = configFile;
-	}
-
-	@EventHandler
-	public void onFeed(PlayerInteractAtEntityEvent e) {
-		if (e.getRightClicked() instanceof Sheep) {
-			Player player = e.getPlayer();
-			if (player.getItemInHand().getType() == Material.TNT){
-			if (Main.getMain().getConfig().getBoolean("explozsivesheep")) {
-					e.getRightClicked().remove();
-					e.getPlayer().getInventory().removeItem(new ItemStack(Material.TNT, 1));
-					Sheep sheep = (Sheep) e.getRightClicked().getLocation().getWorld()
-							.spawnEntity(e.getRightClicked().getLocation(), EntityType.SHEEP);
-					sheep.setColor(DyeColor.RED);
-					sheep.setCustomName(cfg.SHEEP_NAME);
-					sheep.setCustomNameVisible(true);
-					new BukkitRunnable() {
-
-						@Override
-						public void run() {
-							sheep.remove();
-							sheep.getLocation().getWorld().spawnEntity(sheep.getLocation(), EntityType.PRIMED_TNT);
-						}
-					}.runTaskLater(Main.getMain(), 100);
-				}else {
-				e.getPlayer().sendMessage(cfg.DISABLE_SHEEP_MESSGAE);
-			}
-			}
+		public FeedSheepTNTListner(ConfigFile configFile) {
+			this.cfg = configFile;
 		}
 
+		@EventHandler
+		public void onFeed(PlayerInteractAtEntityEvent e) {
+			if (e.getRightClicked() instanceof Sheep) {
+				Player player = e.getPlayer();
+				if (player.getItemInHand().getType() == Material.TNT){
+					if (Main.getMain().getConfig().getBoolean("explozsivesheep")) {
+						e.getPlayer().getInventory().removeItem(new ItemStack(Material.TNT,1));
+						Sheep sheep = (Sheep) e.getRightClicked();
+						sheep.setColor(DyeColor.RED);
+						sheep.setCustomName(cfg.SHEEP_NAME);
+						sheep.setCustomNameVisible(true);
+						new BukkitRunnable() {
+
+							@Override
+							public void run() {
+								sheep.remove();
+								sheep.getLocation().getWorld().spawnEntity(sheep.getLocation(), EntityType.PRIMED_TNT);
+							}
+						}.runTaskLater(Main.getMain(), 100);
+					}else {
+						e.getPlayer().sendMessage(cfg.DISABLE_SHEEP_MESSGAE);
+					}
+				}
+			}
+
+		}
 	}
-}
